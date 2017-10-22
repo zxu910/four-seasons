85d6b25dbc9c68232e0f81bd75a7218a34ae1998
zxu910 <31030089+zxu910@users.noreply.github.com>
2017-10-22 11:08:43 +0800

    01

---
+++ index.js
@@ -0,0 +1,53 @@
+function change(i){
+    
+                if(i==1){
+                    document.body.style.backgroundColor="#FFE4E1";
+                    document.getElementById("slide-bar-spring").style.display="block";
+                    document.getElementById("slide-bar-summer").style.display="none";
+                    document.getElementById("slide-bar-autumn").style.display="none";
+                    document.getElementById("slide-bar-winter").style.display="none";
+                    document.getElementById("the-spring-discription").style.display="block";
+                    document.getElementById("the-summer-discription").style.display="none";
+                    document.getElementById("the-autumn-discription").style.display="none";
+                    document.getElementById("the-winter-discription").style.display="none";
+                }
+    
+                if(i==2){
+                    document.body.style.backgroundColor="#87CEEB";
+                    document.getElementById("slide-bar-spring").style.display="none";
+                    document.getElementById("slide-bar-summer").style.display="block";
+                    document.getElementById("slide-bar-autumn").style.display="none";
+                    document.getElementById("slide-bar-winter").style.display="none";
+                    document.getElementById("the-spring-discription").style.display="none";
+                    document.getElementById("the-summer-discription").style.display="block";
+                    document.getElementById("the-autumn-discription").style.display="none";
+                    document.getElementById("the-winter-discription").style.display="none";
+                }
+                if(i==3){
+                    document.body.style.backgroundColor="#FF6347";
+                    document.getElementById("slide-bar-spring").style.display="none";
+                    document.getElementById("slide-bar-summer").style.display="none";
+                    document.getElementById("slide-bar-autumn").style.display="block";
+                    document.getElementById("slide-bar-winter").style.display="none";
+                    document.getElementById("the-spring-discription").style.display="none";
+                    document.getElementById("the-summer-discription").style.display="none";
+                    document.getElementById("the-autumn-discription").style.display="block";
+                    document.getElementById("the-winter-discription").style.display="none";
+                }
+                if(i==4){
+                    document.body.style.backgroundColor="#FFFAFA";
+                    document.getElementById("slide-bar-spring").style.display="none";
+                    document.getElementById("slide-bar-summer").style.display="none";
+                    document.getElementById("slide-bar-autumn").style.display="none";
+                    document.getElementById("slide-bar-winter").style.display="block";
+                    document.getElementById("the-spring-discription").style.display="none";
+                    document.getElementById("the-summer-discription").style.display="none";
+                    document.getElementById("the-autumn-discription").style.display="none";
+                    document.getElementById("the-winter-discription").style.display="block";
+                }
+            }
+                function normal(){
+    
+             
+                    document.body.style.backgroundColor="white";
+                }
